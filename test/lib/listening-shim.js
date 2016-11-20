56d7c1b6eb58dc9e813e99a1fafd2bd0166ee8e5
Gurpreet Atwal <git@gatwal.com>
2016-11-20 00:16:59 -0800

    create tests for listening shim

---
+++ test/lib/listening-shim.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const test = require('ava');
+const shim = require('../../lib/listening-shim');
+
+test('shim', t => {
+
+  function Mock() {}
+  shim.apply(Mock);
+
+  const mock = new Mock();
+  const proto = Object.getPrototypeOf(mock);
+  const descriptor = Object.getOwnPropertyDescriptor(proto, 'listening');
+
+  t.true('listening' in mock);
+
+  t.true(proto.hasOwnProperty('listening'));
+  t.true(proto.propertyIsEnumerable('listening'));
+
+  t.true(descriptor.enumerable);
+  t.true(descriptor.configurable);
+  t.truthy(descriptor.get);
+
+  t.falsy(descriptor.writable);
+  t.falsy(descriptor.set);
+  t.falsy(descriptor.value);
+
+});
+
+test('register - true', t => {
+
+  function Mock() {}
+  shim.register(Mock);
+
+  const mock = new Mock();
+  const proto = Object.getPrototypeOf(mock);
+  const descriptor = Object.getOwnPropertyDescriptor(proto, 'listening');
+
+  t.true('listening' in mock);
+
+  t.true(proto.hasOwnProperty('listening'));
+  t.true(proto.propertyIsEnumerable('listening'));
+
+  t.true(descriptor.enumerable);
+  t.true(descriptor.configurable);
+  t.truthy(descriptor.get);
+
+  t.falsy(descriptor.writable);
+  t.falsy(descriptor.set);
+  t.falsy(descriptor.value);
+
+});
+
+test('register - false', t => {
+
+  function Mock() {}
+  Mock.prototype.listening = 'set';
+
+  shim.register(Mock);
+
+  const mock = new Mock();
+  const proto = Object.getPrototypeOf(mock);
+  const descriptor = Object.getOwnPropertyDescriptor(proto, 'listening');
+
+  t.true('listening' in mock);
+  t.true(proto.hasOwnProperty('listening'));
+
+  t.falsy(descriptor.get);
+
+  t.is(descriptor.value, 'set');
+
+});
