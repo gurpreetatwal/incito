72b39581caa0a5908193b24a5bd8b779a6eb65db
Gurpreet Atwal <git@gatwal.com>
2016-12-22 23:35:38 -0800

    feat(server): add 'url' property to server

--- index.js
+++ index.js
@@ -13,13 +13,25 @@ function incito(arg) {
     instance.listen(0);
   }
 
-  Object.defineProperty(instance, 'port', {
-    enumerable: true,
-    configurable: true,
-    get: () => instance.address().port,
-  });
+  defineGetter(instance, 'port', () => instance.address().port);
+  defineGetter(instance, 'url', () => `https://localhost:${instance.port}`);
 
   return instance;
+}
+
+/**
+ * Add an enumerable, configurable getter fn to an object
+ *
+ * @param {Object} object - what to attach to
+ * @param {String} key - what to attach under
+ * @param {Function} getter - what to attach
+ */
+function defineGetter(object, key, getter) {
+  Object.defineProperty(object, key, {
+    enumerable: true,
+    configurable: true,
+    get: getter,
+  });
 }
 
 module.exports = incito;
