bb5ac5a454af67b326e065d7eb80f926315f9470
Gurpreet Atwal <git@gatwal.com>
2016-12-22 23:35:38 -0800

    feat(server): add 'url' property to server

--- index.js
+++ index.js
@@ -9,17 +9,29 @@ function incito(arg) {
     instance.listen(0);
   }
 
-  Object.defineProperty(instance, 'port', {
-    enumerable: true,
-    configurable: true,
-    get: () => {
-      // .address() will return `null` if the server has not yet emitted the 'listening' event
-      const address = instance.address();
-      return address ? address.port : null;
-    },
+  defineGetter(instance, 'port', () => {
+    // .address() will return `null` if the server has not yet emitted the 'listening' event
+    const address = instance.address();
+    return address ? address.port : null;
   });
+  defineGetter(instance, 'url', () => `http://localhost:${instance.port}`);
 
   return instance;
+}
+
+/**
+ * Add an enumerable, configurable getter fn to an object
+ *
+ * @param {Object} object - what to attach to
+ * @param {String} key - what to attach under
+ * @param {Function} getter - what to attach
+ */
+function defineGetter(object, key, getter) {
+  Object.defineProperty(object, key, {
+    enumerable: true,
+    configurable: true,
+    get: getter,
+  });
 }
 
 module.exports = incito;
--- test/index.js
+++ test/index.js
@@ -6,9 +6,12 @@ const incito = require('..');
 
 test('incito', t => {
 
-  let instance = incito();
+  let instance, descriptor;
+
+  instance = incito();
   t.true(instance instanceof net.Server);
   t.truthy(instance.port);
+  t.is(instance.host, '::');
   t.true(instance.listening);
 
   const server = net.createServer();
@@ -17,9 +20,18 @@ test('incito', t => {
   instance = incito(server);
   t.true(instance instanceof net.Server);
   t.is(instance.port, 34532);
+  t.is(instance.url, 'https://localhost:34532');
   t.true(instance.listening);
 
-  const descriptor = Object.getOwnPropertyDescriptor(instance, 'port');
+  descriptor = Object.getOwnPropertyDescriptor(instance, 'port');
+  t.true(descriptor.enumerable);
+  t.true(descriptor.configurable);
+  t.truthy(descriptor.get);
+  t.falsy(descriptor.writable);
+  t.falsy(descriptor.value);
+  t.falsy(descriptor.set);
+
+  descriptor = Object.getOwnPropertyDescriptor(instance, 'url');
   t.true(descriptor.enumerable);
   t.true(descriptor.configurable);
   t.truthy(descriptor.get);
