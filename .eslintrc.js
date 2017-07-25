ac84f1fa3d27879fdbdb6de11e6dbe6806d0eb4d
Gurpreet Atwal <git@gatwal.com>
2017-07-24 23:07:36 -0700

    style: add more lint rules & fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,14 @@ module.exports = {
   extends: 'eslint:recommended',
   env: {
     es6: true,
-    node: true
+    node: true,
+  },
+  rules: {
+    semi: 'error',
+    eqeqeq: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
   },
 };
